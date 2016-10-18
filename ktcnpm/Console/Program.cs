2c784dde7eb2cfc5fbaf2763b4072206b702791c
Đỗ Giáp Linh <dogiaplinh_94@live.com>
2016-10-18 22:34:36 +0700

    Rename project, add NPV function

--- ktcnpm/Console/Program.cs
+++ ktcnpm/Console/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Console
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double[] costs = new double[] { 250000 };
+            double[] revenues = new double[] { 0, 70000, 70000, 70000, 70000, 70000 };
+            double rate;
+            for (int i = 0; i < 20; i++)
+            {
+                rate = i + 1;
+                Console.WriteLine($"Rate = {rate:##}\tNPV = {Core.Npv.CalculateNpv(costs, revenues, rate)}");
+            }
         }
     }
 }
---
+++ ktcnpm/Core/Npv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class Npv
+    {
+        public static double CalculateNpv(double[] costs, double[] revenues, double rate)
+        {
+            double cost = 0;
+            var scale = 1 + rate / 100;
+            for (int i = costs.Length - 1; i >= 0; i--)
+            {
+                cost /= scale;
+                cost -= costs[i];
+            }
+            double revenue = 0;
+            for (int i = revenues.Length - 1; i >= 0; i--)
+            {
+                revenue /= scale;
+                revenue += revenues[i];
+            }
+            return cost + revenue;
+        }
+    }
+}
